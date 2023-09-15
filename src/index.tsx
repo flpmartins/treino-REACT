18cb86d7506c3dde71f4af42d8f38bb76ed59620
Filipe Martins <filipemartins0303@gmail.com>
2023-09-15 15:53:23 -0300

    configurando a primeira pagina

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App';
+import { App } from './app/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
