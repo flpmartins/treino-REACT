eb6047bfb512fda66b6017b33a913f6fbc60fab8
Filipe Martins <filipemartins0303@gmail.com>
2023-09-20 14:28:13 -0300

    aprendendo os principais hooks

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom'
 
+import { useRef } from 'react'
+
 export const Dashboard = () => {
-return (
+
+  const counterRef = useRef({couter:0})
+
+  return (
 
  <div>
-   <p>dashboard</p>
-   <Link to="/pagina-login">Login</Link>
- 
+  <p>dashboard</p>
+
+  <p>{counterRef.current.couter}</p>
+
+  <button onClick={() => counterRef.current.couter++}>somar</button>
+  <button onClick={() => console.log(counterRef.current.couter)}>log</button>
+
+    <Link to="/pagina-login">Login</Link>
+   
  </div>
 )
 }
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,27 +1,47 @@
-import { useState } from 'react'
+import {  useMemo, useCallback, useState, useRef } from 'react'
 
-export const Login = () => {
+  export const Login = () => {
+
+  const imputPasswordRef = useRef<HTMLInputElement>(null)
 
   const [email, setEmail] = useState('')
- const  [password, setPassword] = useState('')
+  const  [password, setPassword] = useState('')
 
-  const handleEntrar = () => {
+  const emailLenght = useMemo(() => {
+    return email.length * 1000
+  }, [email.length])
+
+  const handleEntrar = useCallback(() => {
     console.log(email)
     console.log(password)
-
-  }
+  },[email, password]);
 return (
-  <div>
+  <div >
   <form>
+    <p>Quantidades de caracters no email : {emailLenght}</p>
+
     <label>
       <span>Email</span>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)}/> 
-
+      <input
+        type="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        onKeyDown = {e => e.key === 'enter'? imputPasswordRef.current?.focus(): undefined}
+       /> 
+  
       <span>password</span>
-      <input type="email" value={password} onChange={e => setPassword(e.target.value)}/> 
+      <input
+        type="password"
+        value={password}
+        ref={imputPasswordRef}
+        onChange={e => setPassword(e.target.value)}
+      /> 
     </label>
-    <button type="button" onClick={handleEntrar}>entrar </button>
-  </form>
-  </div>
+
+         <button type="button" onClick={handleEntrar}>
+      to enter 
+        </button>
+     </form>
+  </div> 
 )
 }
